53103a1b0a02089598fa65bcf651a41a785b082e
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-06-15 22:46:09 +0600

    Add

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,59 +1,33 @@
 function activateSection(section) {
-    var caddy = document.querySelector('.caddy');
-    var golfer = document.querySelector('.golfer');
-    
-    if (section === 'caddy') {
-      caddy.classList.add('active');
-      golfer.classList.remove('active');
-    } else if (section === 'golfer') {
-      caddy.classList.remove('active');
-      golfer.classList.add('active');
-    }
+  var caddy = document.querySelector('.caddy');
+  var golfer = document.querySelector('.golfer');
+
+  if (section === 'caddy') {
+    caddy.classList.add('active');
+    golfer.classList.remove('active');
+  } else if (section === 'golfer') {
+    caddy.classList.remove('active');
+    golfer.classList.add('active');
+  }
 }
-  
-// Phone Number
 
-const dropdownContainer = document.querySelector('.dropdown-container');
-    const selectedText = document.querySelector('.selected-text');
-    const selectedIcon = document.querySelector('.selected-icon');
-    const dropdownList = document.querySelector('.dropdown-list');
-    const dropdownItems = Array.from(document.querySelectorAll('.dropdown-item'));
+const listItems = document.querySelectorAll(".list");
 
-    dropdownContainer.addEventListener('click', function () {
-      dropdownList.classList.toggle('open');
-    });
+listItems.forEach((item) => {
+  const payer = item.querySelector(".payer");
 
-    dropdownItems.forEach(function (item) {
-      item.addEventListener('click', function () {
-        const value = item.getAttribute('data-value');
-        const text = item.textContent.trim();
-        const iconClass = item.querySelector('.flag-icon').classList[1];
-        selectedText.textContent = text;
-        selectedIcon.className = `flag-icon ${iconClass} selected-icon`;
-        dropdownList.classList.remove('open');
+  payer.addEventListener("click", () => {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+      item.querySelector(".fa-circle-check").style.display = "none";
+    } else {
+      listItems.forEach((otherItem) => {
+        otherItem.classList.remove("active");
+        otherItem.querySelector(".fa-circle-check").style.display = "none";
       });
-    });
 
-    function handleInput(input, nextInput, prevInput) {
-      if (input.value.length === 1) {
-        nextInput.focus();
-      } else if (input.value.length === 0) {
-        prevInput.focus();
-      }
+      item.classList.add("active");
+      item.querySelector(".fa-circle-check").style.display = "inline-block";
     }
-
-    function handleInputTwo(input, nextInput, prevInput) {
-      if (input.value.length === 1) {
-        nextInput.focus();
-      } else if (input.value.length === 0) {
-        prevInput.focus();
-      }
-    }
-
-    function handleInputThree(input, nextInput, prevInput) {
-      if (input.value.length === 1) {
-        nextInput.focus();
-      } else if (input.value.length === 0) {
-        prevInput.focus();
-      }
-    }
+  });
+});
